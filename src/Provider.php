df7a5a29200478047cffa2c0c4909efaa3393a44
SteeinPassport <steein.passport@gmail.com>
2017-07-14 11:37:52 +0300

    Update version v1.1

--- src/Provider.php
+++ src/Provider.php
@@ -14,20 +14,6 @@ class Provider extends AbstractProvider implements ProviderInterface
     const IDENTIFIER = 'STEEIN';
 
     /**
-     * The base Steein URL.
-     *
-     * @var string
-     */
-    protected $urlApi = 'https://www.steein.ru';
-
-    /**
-     * The API version for the request.
-     *
-     * @var string
-     */
-    protected $version = 'v2.0';
-
-    /**
      * {@inheritdoc}
      */
     protected $scopes = ['users', 'email'];
@@ -58,7 +44,7 @@ class Provider extends AbstractProvider implements ProviderInterface
      */
     protected function getUserByToken($token)
     {
-        $response = $this->getHttpClient()->get('https://www.steein.ru/api/'.$this->version.'/users/show', [
+        $response = $this->getHttpClient()->get('https://www.steein.ru/api/v2.0/users/show', [
             'headers' => [
                 'Authorization' => 'Bearer '.$token,
             ],
@@ -74,34 +60,15 @@ class Provider extends AbstractProvider implements ProviderInterface
     {
         return (new User())->setRaw($user)->map([
             'id' => $user['id'],
-            'first_name' => array_get($user['name'], 'last_name'),
+            'first_name' => array_get($user['name'], 'first_name'),
             'last_name' => array_get($user['name'], 'last_name'),
-            'username' => array_get($user, 'username'),
+            'nickname' => array_get($user, 'username'),
             'avatar' => array_get($user, 'avatar'),
+            'name'  =>  array_get($user, 'displayName'),
+            'email' => (isset($user['email']) ? $user['email'] : null)
         ]);
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function user()
-    {
-        if ($this->hasInvalidState()) {
-            throw new InvalidStateException();
-        }
-        $user = $this->mapUserToObject($this->getUserByToken(
-            $token = $this->getAccessTokenResponse($this->getCode())
-        ));
-        return $user->setToken(array_get($token, 'access_token'));
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function parseAccessToken($body)
-    {
-        return json_decode($body, true);
-    }
 
     /**
      * {@inheritdoc}
